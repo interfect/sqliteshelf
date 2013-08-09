bbd048094a40bec6e4981af84bf4d6399865a9f2
Adam Novak <anovak@soe.ucsc.edu>
2013-08-08 21:08:05 -0700

    Added the ability for SQLiteDicts to be "lazy" and not commit to disk until sync is called, just like on a normal shelf.

--- sqliteshelf.py
+++ sqliteshelf.py
@@ -68,19 +68,52 @@ try:
 except ImportError:
     import pickle
 
-import sqlite3
+import sqlite3, threading
 
 class SQLiteDict(DictMixin):
-    def __init__(self, filename=':memory:', table='shelf', flags='r', mode=None):
+    def __init__(self, filename=':memory:', table='shelf', flags='r', mode=None, lazy=False):
+        """
+        Make a new SQLite-backed dict that holds strings. filename specifies the
+        file to use; by default a special filename of ":memory:" is used to keep
+        all data in memory. table specifies the name of the table to use;
+        multiple tables can be used in the same file at the same time. flags and
+        mode exist to match the shelve module's interface and are ignored. By
+        default, data is saved to disk after every operation; passing lazy=True
+        will require you to call the sync() and close() methods for changes to
+        be guaranteed to make it to disk.
+        
+        Note that this object is not thread safe. It is willing to be used by
+        multiple threads, but access should be controlled by a lock.
+        
+        """
+        
         self.table = table
+        self.lazy = lazy 
         MAKE_SHELF = 'CREATE TABLE IF NOT EXISTS '+self.table+' (key TEXT, value TEXT)'
         MAKE_INDEX = 'CREATE UNIQUE INDEX IF NOT EXISTS '+self.table+'_keyndx ON '+self.table+'(key)'
-        self.conn = sqlite3.connect(filename)
+        self.conn = sqlite3.connect(filename, check_same_thread=False)
         self.conn.text_factory = str
         self.conn.execute(MAKE_SHELF)
         self.conn.execute(MAKE_INDEX)
-        self.conn.commit()
+        self.maybe_sync()
 
+    def maybe_sync(self):
+        """
+        Sync to disk if this SqliteDict is not lazy.
+        
+        """
+
+        if not self.lazy:
+            # We're not lazy. Commit now!
+            self.conn.commit()
+            
+    def sync(self):
+        """
+        Sync to disk. Finishes any internal sqlite transactions.
+        
+        """
+        self.conn.commit()
+            
     def __getitem__(self, key):
         GET_ITEM = 'SELECT value FROM '+self.table+' WHERE key = ?'
         item = self.conn.execute(GET_ITEM, (key,)).fetchone()
@@ -91,14 +124,14 @@ class SQLiteDict(DictMixin):
     def __setitem__(self, key, item):
         ADD_ITEM = 'REPLACE INTO '+self.table+' (key, value) VALUES (?,?)'
         self.conn.execute(ADD_ITEM, (key, item))
-        self.conn.commit()
+        self.maybe_sync()
 
     def __delitem__(self, key):
         if key not in self:
             raise KeyError(key)
         DEL_ITEM = 'DELETE FROM '+self.table+' WHERE key = ?'
         self.conn.execute(DEL_ITEM, (key,))
-        self.conn.commit()
+        self.maybe_sync()
 
     def __iter__(self):
         c = self.conn.cursor()
